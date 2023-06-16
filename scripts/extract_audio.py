0b1f45c71c62f7d649c2362bdd639d064609cdb6
albeltra <alexbeltran@berkeley.edu>
2023-06-16 12:43:25 -0700

    more changes

--- scripts/extract_audio.py
+++ scripts/extract_audio.py
@@ -8,13 +8,14 @@ mediaFile = ast.literal_eval(os.environ.get('mediaFile'))
 mediaInfo = ast.literal_eval(os.environ.get('mediaInfo'))
 audio_channel = os.environ.get('audio_channel', '0')
 
-print(mediaFile)
-
 extract_audio = MediaHelper().extract_audio
 
+output_file_path = "/data/" + Path(mediaFile['path']).name + '.wav'
+
 kwargs = {'video_file_path':  mediaFile['path'],
-          'TEMP_DIR_PATH': "/TEMP-SUBS/" + Path(mediaFile['path']).name + '.wav',
+          'TEMP_DIR_PATH': output_file_path,
           'channel': audio_channel,
           'decompress': True}
 
-extract_audio(**kwargs)
+if not os.path.exists(output_file_path):
+    extract_audio(**kwargs)
